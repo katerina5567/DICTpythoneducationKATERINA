a4f952117b26d74137b4f770184d43351521f2cd
katerina5567 <kati.allam2045@gmail.com>
2025-10-07 11:19:12 +0300

    Zoo 4-th stage

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -1,52 +1,36 @@
 # Zoo/zoo.py
 
-# ===== Етап 1 та 2: Привітання та введення імені =====
-bot_name = "DICT_Bot"
-birth_year = "2025"
+# ===== Етап 1: Привітання =====
+zoo_name = "MyZoo"
+print(f"Welcome to {zoo_name}!")
+print("Here you can see and manage the animals in our zoo.")
 
-print(f"Hello! My name is {bot_name}.")
-print(f"I was created in {birth_year}.")
-print("Please, remind me your name.")
+# ===== Етап 2: Створення списку тварин =====
+animals = []  # порожній список тварин
+print("Currently, there are no animals in the zoo.")
 
-user_name = input("> ")  # користувач вводить своє ім’я
-print(f"What a great name you have, {user_name}!")
-
-# ===== Етап 3: Вгадування віку =====
-print("Let me guess your age.")
-print("Enter remainders of dividing your age by 3, 5 and 7.")
-
-remainder3 = int(input("> "))
-remainder5 = int(input("> "))
-remainder7 = int(input("> "))
-
-age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-print(f"Your age is {age}; that's a good time to start programming!")
-
-# ===== Етап 4: Підрахунок =====
-print("Now I will prove to you that I can count to any number you want.")
-num = int(input("> "))
-
-for i in range(num + 1):
-    print(f"{i} !")
-
-print("Completed, have a nice day!")
-
-# ===== Етап 5: Тестування =====
-print("Let's test your programming knowledge.")
-print("Why do we use methods?")
-print("1. To repeat a statement multiple times.")
-print("2. To decompose a program into several small subroutines.")
-print("3. To determine the execution time of a program.")
-print("4. To interrupt the execution of a program.")
-
-correct_answer = 2
-
+# ===== Етап 3: Додавання та перегляд тварин =====
 while True:
-    answer = int(input("> "))
-    if answer == correct_answer:
+    print("\nChoose an action:")
+    print("1. Add an animal")
+    print("2. Show all animals")
+    print("3. Exit")
+    
+    choice = input("> ")
+    
+    if choice == "1":
+        animal_name = input("Enter animal name: ")
+        animals.append(animal_name)
+        print(f"{animal_name} has been added to the zoo.")
+    elif choice == "2":
+        if animals:
+            print("Animals in the zoo:")
+            for i, animal in enumerate(animals, start=1):
+                print(f"{i}. {animal}")
+        else:
+            print("The zoo is empty.")
+    elif choice == "3":
+        print("Goodbye! Thanks for visiting the zoo.")
         break
     else:
-        print("Please, try again.")
-
-print("Completed, have a nice day!")
-print("Congratulations, have a nice day!")
+        print("Invalid choice. Please try again.")
