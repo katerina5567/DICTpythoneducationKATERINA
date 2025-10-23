e8f80b619b45936104274413b1ac8d0443fe6585
katerina5567 <kati.allam2045@gmail.com>
2025-10-23 20:56:36 +0300

    Temp commit: include latest changes

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,6 +1,6 @@
 import random
 
-# Этап 1: Добавляем друзей
+
 num_friends = int(input("Enter the number of friends joining (including you):\n> "))
 if num_friends <= 0:
     print("No one is joining for the party")
@@ -11,13 +11,13 @@ else:
         name = input("> ")
         friends[name] = 0
 
-    # Этап 2: Разделяем счёт поровну
+   
     total_amount = float(input("Enter the total amount:\n> "))
     split_amount = round(total_amount / num_friends, 2)
     for name in friends:
         friends[name] = split_amount
 
-    # Этап 3 и 4: Выбор щасливчика
+   
     use_lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n> ')
     if use_lucky == "Yes":
         lucky_one = random.choice(list(friends.keys()))
@@ -30,11 +30,11 @@ else:
                 else:
                     friends[name] = 0
         else:
-            # Если только один участник, он сам счастливчик
+        
             friends[lucky_one] = 0
     else:
         print("No one is going to be lucky")
 
-    # Вывод словаря
+   
     print(friends)
 
